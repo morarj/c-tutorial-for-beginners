a6925b03f862bb5f1a3a970636955b6065f764a1
Hugo Morales Arjón <hugo_morarj@hotmail.com>
2016-09-25 02:09:34 -0500

    Add notes and exercises of section 3

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b;
+            int i;
+
+            // Implicit
+            b = 1;
+            i = b;
+            Console.WriteLine(i);
+
+            // Explicit
+            i = 1;
+            b = (byte) i;
+            Console.WriteLine(b);
+
+            i = 1000;
+            b = (byte)i;
+            Console.WriteLine(b);
+
+            string str = "1234";
+            i = Convert.ToInt32(str);
+            Console.WriteLine(i);
+
+            try
+            {
+                b = Convert.ToByte(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+
+            try
+            {
+                str = "true";
+                bool boolean = Convert.ToBoolean(str);
+                Console.WriteLine(boolean);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 10.5f;
+            char character = 'A';
+            string firstName = "Hugo";
+            bool isWorking = true;
+            var test = "Testing";
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(test);
+        }
+    }
+}
